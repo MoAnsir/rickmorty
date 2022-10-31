dd95349feb347c35a3e58de828ef60d7985e6af8
Mohammed Ansr <mohammed.ansir@gmail.com>
2022-10-31 07:50:05 +0000

    styling setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", "dark");
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto px-4">
+      <h1 className="text-4xl text-center">Rick & Morty</h1>
     </div>
   );
 }
